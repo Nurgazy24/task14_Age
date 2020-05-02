07660b2626d6b79d3cc9e0b41d0194a63424143d
Nurgazy24 <nuroskonbaev24ray@gmail.com>
2020-05-02 21:11:05 +0600

    Add task14Age file

---
+++ task14Age.py
@@ -0,0 +1,16 @@
+age = 32 
+b = []
+c = []
+for item in range(0, 33):
+    if item % 2 != 0:
+        b.append(item)      
+    else:
+        c.append(item)
+print(b)
+print(c)
+      
+
+    
+       
+
+    
